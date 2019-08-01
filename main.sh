3ce9070827f7d3325b3ab850477ee58de1e63f16
Ginni Hema Sundara Rao <hema.ginni@inmar.com>
2019-08-01 21:52:11 +0530

    Added git ignore and added script for cron

---
+++ main.sh
@@ -0,0 +1,4 @@
+cd /home/hemant0328/practice/meme-robot
+env/bin/python3 main.py
+
+#*/30 * * * * /home/hemant0328/practice/meme-robot/main.sh >> /home/hemant0328/practice/meme-robot/logs/`(date +"%H:%M,%d-%b-%Y")`.log
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+# List of dependencies installed via `pip install -e .`
+# by virtue of the Setuptools `install_requires` value below.
+requires = [
+    'requests',
+    'selenium',
+    'lxml',
+    'cssselect',
+    'Pillow'
+]
+setup(
+    name='meme-robot',
+    install_requires=requires,
+)
